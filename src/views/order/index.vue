371d9d5706e9f7fbde028d4eee38ff38a329c5d8
AndrewZ <1599118717@qq.com>
2020-09-06 22:17:40 +0800

    确认订单

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -22,18 +22,19 @@
 				<span><b>{{ merchantName }}</b></span>
 			</div>
 			<van-row>
-				<van-col span="12">
+				<van-col span="8">
 					<span class="info">订单配送至</span>
 				</van-col>
-				<van-col span="12">
-					<span class="info" @click="addrSelectVisible = true">{{ addressNow }}&nbsp;></span>
+				<van-col span="16">
+					<span class="info info-value van-ellipsis" style="color:#57a6fc;" @click="addrSelectVisible = true">{{ addressNow }}<van-icon name="arrow" /></span>
 					<van-action-sheet
 						v-model="addrSelectVisible"
 						:actions="addrs"
-						title="请选择收货地址"
+						title="选择收货地址"
 						close-on-click-action
 						@select="addrSelecter"
-					/>
+					>
+					</van-action-sheet>
 				</van-col>
 			</van-row>
 			<van-row>
@@ -41,7 +42,7 @@
 					<span class="info">{{ contact }}</span>
 				</van-col>
 				<van-col span="12">
-					<span class="info">{{ phone }}</span>
+					<span class="info info-value">{{ phone }}</span>
 				</van-col>
 			</van-row>
 			<van-row>
@@ -49,7 +50,7 @@
 					<span class="info">配送时间：</span>
 				</van-col>
 				<van-col span="12">
-					<span class="info">2020-08-01 晚餐</span>
+					<span class="info info-value">2020-08-01 晚餐</span>
 				</van-col>
 			</van-row>
 			<van-row>
@@ -57,11 +58,11 @@
 					<span class="info">支付方式：</span>
 				</van-col>
 				<van-col span="12">
-					<span class="info">货到付款</span>
+					<span class="info info-value">货到付款</span>
 				</van-col>
 			</van-row>
 		</div>
-		<div class="footer">
+		<div class="footer van-hairline--surround">
 			<van-card
 			  num="2"
 			  price="2.00"
@@ -84,15 +85,20 @@
 			>
 			</van-card>
 			<van-row>
-				<van-col :span="8">
-					总金额：
+				<van-col :span="12">
+					<span class="total-text"><b>总金额：</b></span>
 				</van-col>
-				<van-col :span="8">
-					￥297
+				<van-col :span="12">
+					<div class="total-value">
+						<span class="total-symbol">￥</span>
+						<span class="total-num">297</span>
+					</div>
 				</van-col>
 			</van-row>
 		</div>
-		<van-button>提交订单</van-button>
+		<div class="submit-button">
+			<van-button class="submit">提交订单</van-button>
+		</div>
 	</div>
 </template>
 
@@ -107,7 +113,7 @@ export default {
 			addrSelectVisible: false,
 			addressNow: '地址选择',
 			addrs: [
-				{ name: '嘉定区人民街200弄' },
+				{ name: '嘉定区人民街200弄asdfasdfasdfasfasdfsadfasasdfasdf' },
 				{ name: '嘉定区人民街201弄' }
 			]
 		}
@@ -149,26 +155,65 @@ export default {
 	font-size: 36px;
 }
 
-.user-info {
+.user-info, .footer, .submit-button {
 	margin: 0 auto;
-	width: 95%;
+	width: 94%;
 	background-color: white;
-	border-radius: 10px;
-	padding: 20px 16px;
+	border-radius: 15px;
 	margin-top: 15px;
+	box-shadow: 0 0 1px 1px #efefef;
+}
+
+.user-info, .footer {
+	padding: 20px 30px;
 }
 
 .merchantName {
 	font-size: 42px;
 	margin: 20px 0;
+	letter-spacing: 2px;
 }
 
 .van-row {
-	margin: 20px 0;
+	margin: 30px 0;
 }
 
 span.info {
 	font-size: 30px;
+	letter-spacing: 1px;
+	color: #333333;
 }
 
+span.info-value {
+	display: block;
+	text-align: right;
+}
+
+span.total-text {
+	font-size: 48px;
+}
+
+div.total-value {
+	color: #fb5443;
+	display: block;
+	text-align: right;
+}
+
+span.total-symbol {
+	font-size: 28px;
+}
+
+span.total-num {
+	font-size: 48px;
+}
+
+.submit {
+	width: 100%;
+	font-size: 32px;
+	color: white;
+	background-color: #49b5e9;
+	padding: 50px;
+	margin-top: 10px;
+	border-radius: 10px;
+}
 </style>
