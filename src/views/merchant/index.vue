2b951184317dc9c9dfbe13751b9d3ef78d53a020
chloee <775198041@qq.com>
2020-10-24 22:13:04 +0800

    eatType

--- src/views/merchant/index.vue
+++ src/views/merchant/index.vue
@@ -136,8 +136,8 @@ export default {
 			shopName: "",
 			// 获取菜单查询
 			queryInfo: {
-				shopId: this.$route.params.shopId,
-				eatType: this.$route.params.eatType,
+				shopId: this.$route.query.shopId,
+				eatType: this.$route.query.eatType,
 				orgId: this.$store.getters.orgId
 			},
 			// 当前选项卡名称
@@ -335,9 +335,9 @@ export default {
 	created() {
 		// 添加到缓存视图
 		this.$store.dispatch('cacheViews/addCachedView', this.$route)
-		this.$store.dispatch('order/updateShopId', this.$route.params.shopId)
+		this.$store.dispatch('order/updateShopId', this.$route.query.shopId)
 
-		this.shopName = this.$route.params.shopName
+		this.shopName = this.$route.query.shopName
 		this.$nextTick(() => {
 		  this._calculateHeight()
 		})
